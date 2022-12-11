4e4d086684cf69eaf0a533765983d0e5bfd7e3f9
KuangPF <1633397286@qq.com>
2022-12-11 17:54:50 +0800

    chore(eslint): update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,3 @@
 module.exports = {
-  extends: ['@utopia/eslint-config-react', '@utopia/eslint-config-react/hooks'],
-  rules: {
-    'react/require-default-props': 0
-  }
+  extends: ['@utopia/eslint-config-react', '@utopia/eslint-config-react/hooks']
 };
