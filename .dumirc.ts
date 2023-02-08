bd4f8cebd992365b7ac0bb734c179f1919b289d0
KuangPF <1633397286@qq.com>
2023-02-08 21:37:35 +0800

    chore(deps): update dumi-theme-antd version

--- .dumirc.ts
+++ .dumirc.ts
@@ -4,24 +4,22 @@ export default defineConfig({
   outputPath: 'docs-dist',
   themeConfig: {
     name: 'core',
-    antdTheme: {
-      github: 'https://github.com/utopiajs/react-design-core',
-      owner: 'KuangPF | Copyright © 2022-present',
-      sidebarGroupModePath: ['/components'],
-      title: 'React Design Core',
-      description: 'React 组件开发模板，开箱即用',
-      actions: [
-        {
-          type: 'primary',
-          text: '开始使用',
-          link: '/guide'
-        },
-        {
-          text: '组件',
-          link: '/components/copy-right'
-        }
-      ]
-    }
+    github: 'https://github.com/utopiajs/react-design-core',
+    owner: 'KuangPF | Copyright © 2022-present',
+    sidebarGroupModePath: ['/components'],
+    title: 'React Design Core',
+    description: 'React 组件开发模板，开箱即用',
+    actions: [
+      {
+        type: 'primary',
+        text: '开始使用',
+        link: '/guide'
+      },
+      {
+        text: '组件',
+        link: '/components/copy-right'
+      }
+    ]
   },
   styles: ['/styles/reset.css']
 });
